306b35cb31ac3ebc33feb4063f9bc5010121997d
Milos Faktor <faktor.milos@gmail.com>
2026-05-05 18:44:57 +0200

    Add standalone worker process experiment

--- app/services/worker_service.py
+++ app/services/worker_service.py
@@ -1,7 +1,13 @@
+import json
 import time
 
-from app.services.order_service import process_order
-from app.services.queue_service import get_processing_queue
+from app import config
+from app.services.order_service import create_order, process_order
+from app.services.queue_service import (
+    enqueue_order,
+    get_processing_queue,
+    not_queue_empty,
+)
 
 
 def process_next_order():
@@ -12,13 +18,42 @@ def process_next_order():
     return process_order(order_id)
 
 
-def worker():
-    time.sleep(1)
-    return print("worker is running...")
+def process_next_order_worker_server():
+    queue = get_processing_queue()
+    print("Processing order ")
+
+    order_id = queue.pop(0)
+    return json.dumps(process_order(order_id), indent=2)
+
+
+def worker_server():
+    order_id = "order_123"
+    seconds = 0
+    while True:
+        if not_queue_empty():
+            process_next_order_worker_server()
+
+        print(f"[{seconds}]Queue: Empty")
+        if (seconds % 4) == 0:
+            create_order(
+                idempotency_key="5sdfg5sdfg5",
+                order_id=order_id,
+                customer_id="cust_123",
+                items=[
+                    {"sku": "SKU-001", "quantity": 2},
+                    {"sku": "SKU-002", "quantity": 1},
+                ],
+                currency="EUR",
+            )
+            enqueue_order(order_id)
+            print("Queue: Enqueued_order")
+        seconds += config.QUEUE_INTERVAL
+        time.sleep(config.QUEUE_INTERVAL)
 
 
 def main():
-    pass
+    print("Worker has been started.")
+    worker_server()
 
 
 if __name__ == "__main__":
